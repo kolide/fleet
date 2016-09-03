f5bf3b77662e07b379536cdc4fe07ba82d4b7139
Mike Arpaia <mike@arpaia.co>
2016-09-03 17:55:12 -0400

    test stubs

---
+++ kitserver/service_packs_test.go
@@ -0,0 +1,33 @@
+package kitserver
+
+import (
+	"testing"
+)
+
+func TestGetAllPacks(t *testing.T) {
+
+}
+
+func TestGetPack(t *testing.T) {
+
+}
+
+func TestNewPack(t *testing.T) {
+
+}
+
+func TestModifyPack(t *testing.T) {
+
+}
+
+func TestDeletePack(t *testing.T) {
+
+}
+
+func TestAddQueryToPack(t *testing.T) {
+
+}
+
+func TestDeleteQueryFromPack(t *testing.T) {
+
+}
---
+++ kitserver/service_queries_test.go
@@ -0,0 +1,25 @@
+package kitserver
+
+import (
+	"testing"
+)
+
+func TestGetAllQueries(t *testing.T) {
+
+}
+
+func TestGetQuery(t *testing.T) {
+
+}
+
+func TestNewQuery(t *testing.T) {
+
+}
+
+func TestModifyQuery(t *testing.T) {
+
+}
+
+func TestDeleteQuery(t *testing.T) {
+
+}
