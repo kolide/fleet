86bc79f784fc3d78c0648be16b69970467bdec38
Mike Stone <mikestone14@gmail.com>
2016-09-09 17:26:13 -0400

    adds auth to redux state

--- frontend/redux/nodes/auth/actions.js
+++ frontend/redux/nodes/auth/actions.js
@@ -1,3 +1,41 @@
-export const LOGIN = 'LOGIN';
+import APIClient from '../../../utilities/api';
 
-export const userLogin = { type: LOGIN };
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const loginRequest = { type: LOGIN_REQUEST };
+export const loginSuccess = (user) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: {
+      data: user,
+    },
+  };
+};
+export const loginFailure = (error) => {
+  return {
+    type: LOGIN_FAILURE,
+    payload: {
+      error,
+    },
+  };
+};
+
+// formData should be { username: <string>, password: <string> }
+export const loginUser = (formData) => {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      dispatch(loginRequest);
+      APIClient.loginUser(formData)
+        .then(user => {
+          dispatch(loginSuccess(user));
+          return resolve(user);
+        })
+        .catch(error => {
+          dispatch(loginFailure(error.message));
+          return reject(error);
+        });
+    });
+  };
+};
