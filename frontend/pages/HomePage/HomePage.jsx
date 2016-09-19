b3e86d1655f61d3979f286a79395eed701f7d395
Mike Stone <mikestone14@gmail.com>
2016-09-19 15:16:00 -0400

    Adds logout link to homepage

--- frontend/pages/HomePage/HomePage.jsx
+++ frontend/pages/HomePage/HomePage.jsx
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import paths from '../../router/paths';
 
 export class HomePage extends Component {
+  static propTypes = {
+    user: PropTypes.object,
+  };
+
   render () {
-    return <div>Home page</div>;
+    const { user } = this.props;
+    const { LOGOUT } = paths;
+
+    return (
+      <div>
+        Home page
+        {user && <Link to={LOGOUT}>Logout</Link>}
+      </div>
+    );
   }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => {
+  const { user } = state.auth;
+
+  return { user };
+};
+
+export default connect(mapStateToProps)(HomePage);
 
