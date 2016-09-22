ddef6ca3460f1b858b17d3a72bf918a3e2fb76ba
Mike Stone <mikestone14@gmail.com>
2016-09-22 16:50:11 -0400

    Configure user entities

--- frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
+++ frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import entityGetter from '../../../redux/entityGetter';
+import userActions from '../../../redux/nodes/entities/users/actions';
 
 class UserManagementPage extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+    users: PropTypes.arrayOf(PropTypes.object),
+  };
+
+  componentWillMount () {
+    const { dispatch } = this.props;
+    const { load } = userActions;
+
+    dispatch(load());
+  }
+
   render () {
     return (
       <div>
@@ -10,5 +25,11 @@ class UserManagementPage extends Component {
   }
 }
 
-export default UserManagementPage;
+const mapStateToProps = (state) => {
+  const { entities: users } = entityGetter(state).get('users');
+
+  return { users };
+};
+
+export default connect(mapStateToProps)(UserManagementPage);
 
