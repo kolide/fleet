09c91ac47933a1f944517b00b254bd2beb64bf72
Mike Stone <mikestone14@gmail.com>
2016-09-26 13:37:21 -0400

    Move rendering individual users to its own component

--- frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
+++ frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
@@ -1,34 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Avatar from '../../../components/Avatar';
-import GradientButton from '../../../components/buttons/GradientButton';
 import componentStyles from './styles';
-import Dropdown from '../../../components/forms/fields/Dropdown';
 import entityGetter from '../../../redux/entityGetter';
+import GradientButton from '../../../components/buttons/GradientButton';
 import userActions from '../../../redux/nodes/entities/users/actions';
+import UserBlock from './UserBlock';
 
 class UserManagementPage extends Component {
   static propTypes = {
     dispatch: PropTypes.func,
     users: PropTypes.arrayOf(PropTypes.object),
   };
-
-  static userActionOptions = (user) => {
-    const userEnableAction = user.enabled
-      ? { text: 'Disable Account', value: 'disable_account' }
-      : { text: 'Enable Account', value: 'enable_account' };
-    const userPromotionAction = user.admin
-      ? { text: 'Demote User', value: 'demote_user' }
-      : { text: 'Promote User', value: 'promote_user' };
-
-    return [
-      { text: 'Actions...', value: '' },
-      userEnableAction,
-      userPromotionAction,
-      { text: 'Require Password Reset', value: 'reset_password' },
-      { text: 'Modify Details', value: 'modify_details' },
-    ];
-  }
 
   componentWillMount () {
     const { dispatch, users } = this.props;
@@ -39,80 +21,38 @@ class UserManagementPage extends Component {
     return false;
   }
 
-  onUserActionSelect = (user) => {
-    return (formData) => {
-      const { dispatch } = this.props;
+  onUserActionSelect = (user, formData) => {
+    const { dispatch } = this.props;
 
-      if (formData.user_actions) {
-        switch (formData.user_actions) {
-          case 'demote_user':
-            return dispatch(userActions.update(user, { admin: false }));
-          case 'disable_account':
-            return dispatch(userActions.update(user, { enabled: false }));
-          case 'enable_account':
-            return dispatch(userActions.update(user, { enabled: true }));
-          case 'promote_user':
-            return dispatch(userActions.update(user, { admin: true }));
-          case 'reset_password':
-            return dispatch(userActions.update(user, { force_password_reset: true }));
-          default:
-            return false;
-        }
+    if (formData.user_actions) {
+      switch (formData.user_actions) {
+        case 'demote_user':
+          return dispatch(userActions.update(user, { admin: false }));
+        case 'disable_account':
+          return dispatch(userActions.update(user, { enabled: false }));
+        case 'enable_account':
+          return dispatch(userActions.update(user, { enabled: true }));
+        case 'promote_user':
+          return dispatch(userActions.update(user, { admin: true }));
+        case 'reset_password':
+          return dispatch(userActions.update(user, { force_password_reset: true }));
+        default:
+          return false;
       }
+    }
 
-      return false;
-    };
+    return false;
   }
 
   renderUserBlock = (user) => {
-    const {
-      avatarStyles,
-      nameStyles,
-      userDetailsStyles,
-      userEmailStyles,
-      userHeaderStyles,
-      userLabelStyles,
-      usernameStyles,
-      userPositionStyles,
-      userStatusStyles,
-      userStatusWrapperStyles,
-      userWrapperStyles,
-    } = componentStyles;
-    const {
-      admin,
-      email,
-      enabled,
-      name,
-      position,
-      username,
-    } = user;
-    const userLabel = admin ? 'Admin' : 'User';
-    const activeLabel = enabled ? 'Active' : 'Disabled';
-    const userActionOptions = UserManagementPage.userActionOptions(user);
+    const { onUserActionSelect } = this;
 
     return (
-      <div key={email} style={userWrapperStyles}>
-        <div style={userHeaderStyles}>
-          <span style={nameStyles}>{name}</span>
-        </div>
-        <div style={userDetailsStyles}>
-          <Avatar user={user} style={avatarStyles} />
-          <div style={userStatusWrapperStyles}>
-            <span style={userLabelStyles}>{userLabel}</span>
-            <span style={userStatusStyles(enabled)}>{activeLabel}</span>
-            <div style={{ clear: 'both' }} />
-          </div>
-          <p style={usernameStyles}>{username}</p>
-          <p style={userPositionStyles}>{position}</p>
-          <p style={userEmailStyles}>{email}</p>
-          <Dropdown
-            fieldName="user_actions"
-            options={userActionOptions}
-            initialOption={{ text: 'Actions...' }}
-            onSelect={this.onUserActionSelect(user)}
-          />
-        </div>
-      </div>
+      <UserBlock
+        key={user.email}
+        onSelect={onUserActionSelect}
+        user={user}
+      />
     );
   }
 
