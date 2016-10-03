f0de54160076ade8189bdeab9f249625bf8080f3
Mike Stone <mikestone14@gmail.com>
2016-10-03 10:48:37 -0400

    Input field with label component

--- frontend/components/forms/fields/InputFieldWithLabel/InputFieldWithLabel.jsx
+++ frontend/components/forms/fields/InputFieldWithLabel/InputFieldWithLabel.jsx
@@ -3,12 +3,13 @@ import radium from 'radium';
 import componentStyles from './styles';
 import InputField from '../InputField';
 
-class InputFieldWithIcon extends InputField {
+class InputFieldWithLabel extends InputField {
   static propTypes = {
     autofocus: PropTypes.bool,
     defaultValue: PropTypes.string,
     error: PropTypes.string,
     iconName: PropTypes.string,
+    label: PropTypes.string,
     name: PropTypes.string,
     onChange: PropTypes.func,
     placeholder: PropTypes.string,
@@ -26,41 +27,28 @@ class InputFieldWithIcon extends InputField {
     };
   }
 
-  renderHeading = () => {
-    const { error, placeholder } = this.props;
-    const { value } = this.state;
-    const { errorStyles, placeholderStyles } = componentStyles;
-
-    if (error) {
-      return <div style={errorStyles}>{error}</div>;
-    }
-
-    return <div style={placeholderStyles(value)}>{placeholder}</div>;
-  }
-
   render () {
-    const { error, iconName, name, placeholder, style, type } = this.props;
-    const { containerStyles, iconStyles, iconErrorStyles, inputErrorStyles, inputStyles } = componentStyles;
+    const { label, name, placeholder, style, type } = this.props;
+    const { containerStyles, inputStyles, labelStyles } = componentStyles;
     const { value } = this.state;
     const { onInputChange } = this;
 
     return (
-      <div style={containerStyles}>
-        {this.renderHeading()}
+      <div style={[containerStyles, style.container]}>
+        <label htmlFor={name} style={labelStyles}>{label}</label>
         <input
           name={name}
           onChange={onInputChange}
-          className="input-with-icon"
           placeholder={placeholder}
           ref={(r) => { this.input = r; }}
-          style={[inputStyles(value, type), inputErrorStyles(error), style]}
+          style={[inputStyles(value, type), style.input]}
           type={type}
           value={value}
         />
-        {iconName && <i className={iconName} style={[iconStyles(value), iconErrorStyles(error), style]} />}
       </div>
     );
   }
 }
 
-export default radium(InputFieldWithIcon);
+export default radium(InputFieldWithLabel);
+
