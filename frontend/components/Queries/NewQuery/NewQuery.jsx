b57d390974fc62b39ed98c9198486146379c5fe8
Mike Stone <mikestone14@gmail.com>
2016-10-04 13:47:45 -0400

    Implement react-select for selecting query targets

--- frontend/components/Queries/NewQuery/NewQuery.jsx
+++ frontend/components/Queries/NewQuery/NewQuery.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import AceEditor from 'react-ace';
 import 'brace/ext/linking';
 import radium from 'radium';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import './mode';
 import './theme';
 import componentStyles from './styles';
@@ -21,6 +23,27 @@ class NewQuery extends Component {
 
     this.state = {
       saveQuery: false,
+      targetOptions: [
+        {
+          id: 3,
+          label: 'OS X El Capitan 10.11',
+          name: 'osx-10.11',
+          type: 'host',
+        },
+        {
+          id: 4,
+          label: "Jason Meller's Macbook Pro",
+          name: 'jmeller.local',
+          type: 'host',
+        },
+        {
+          id: 4,
+          label: 'All Macs',
+          name: 'macs',
+          type: 'label',
+        }
+      ],
+      selectedTargets: [],
       theme: 'kolide',
     };
   }
@@ -48,6 +71,11 @@ class NewQuery extends Component {
     return false;
   }
 
+  onTargetSelect = (selectedTargets) => {
+    this.setState({ selectedTargets });
+    return false;
+  }
+
   onThemeSelect = (evt) => {
     evt.preventDefault();
 
@@ -72,15 +100,15 @@ class NewQuery extends Component {
     const {
       containerStyles,
       selectTargetsHeaderStyles,
-      targetsInputStyle,
       titleStyles,
     } = componentStyles;
     const { onTextEditorInputChange, textEditorText } = this.props;
-    const { saveQuery, theme } = this.state;
+    const { saveQuery, selectedTargets, targetOptions, theme } = this.state;
     const {
       onBeforeLoad,
       onLoad,
       onSaveQueryFormSubmit,
+      onTargetSelect,
       onThemeSelect,
       onToggleSaveQuery,
     } = this;
@@ -113,7 +141,17 @@ class NewQuery extends Component {
         </div>
         <div>
           <p style={selectTargetsHeaderStyles}>Select Targets</p>
-          <input type="text" style={targetsInputStyle} />
+          <Select
+            className="target-select"
+            multi
+            name="targets"
+            options={targetOptions}
+            onChange={onTargetSelect}
+            placeholder="Type to search"
+            resetValue={[]}
+            value={selectedTargets}
+            valueKey="name"
+          />
         </div>
         <SaveQuerySection onToggleSaveQuery={onToggleSaveQuery} saveQuery={saveQuery} />
         <SaveQueryForm onSubmit={onSaveQueryFormSubmit} saveQuery={saveQuery} />
