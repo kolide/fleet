e339a9d08365d6a4809bab1d86e5b5525b1c8d96
Mike Stone <mikestone14@gmail.com>
2016-10-06 16:28:47 -0400

    New hosts route

---
+++ frontend/pages/hosts/NewHostPage/NewHostPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class NewHostPage extends Component {
+  render () {
+    return (
+      <div>
+        <h1>New Host Page</h1>
+      </div>
+    );
+  }
+}
+
+export default NewHostPage;
