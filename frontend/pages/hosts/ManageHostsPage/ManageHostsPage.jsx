8e7b1b007e22c12dfca0a60e698ce58c10eda08d
Mike Stone <mikestone14@gmail.com>
2016-10-07 12:14:24 -0700

    Adds redux hosts from state to ManageHostsPage

--- frontend/pages/hosts/ManageHostsPage/ManageHostsPage.jsx
+++ frontend/pages/hosts/ManageHostsPage/ManageHostsPage.jsx
@@ -1,6 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import entityGetter from '../../../redux/entityGetter';
+import hostActions from '../../../redux/nodes/entities/hosts/actions';
 
 class ManageHostsPage extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+    hosts: PropTypes.array,
+  };
+
+  componentWillMount () {
+    const { dispatch, hosts } = this.props;
+
+    if (!hosts.length) dispatch(hostActions.loadAll());
+
+    return false;
+  }
+
   render () {
     return (
       <h1>Manage Hosts Page</h1>
@@ -8,4 +25,10 @@ class ManageHostsPage extends Component {
   }
 }
 
-export default ManageHostsPage;
+const mapStateToProps = (state) => {
+  const { entities: hosts } = entityGetter(state).get('hosts');
+
+  return { hosts };
+};
+
+export default connect(mapStateToProps)(ManageHostsPage);
