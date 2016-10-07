c7cea6b4c4a1ed1fa65c2fb59a1fd3232bce9a2b
Mike Stone <mikestone14@gmail.com>
2016-10-07 11:39:24 -0700

    Adds manage hosts page and improves side bar functionality

---
+++ frontend/pages/hosts/ManageHostsPage/ManageHostsPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ManageHostsPage extends Component {
+  render () {
+    return (
+      <h1>Manage Hosts Page</h1>
+    );
+  }
+}
+
+export default ManageHostsPage;
