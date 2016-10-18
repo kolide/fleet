3d1022e5127a12cfb7b6297e41aad8642f2e4fff
Victor Vrantchan <vrancean+github@gmail.com>
2016-10-18 10:25:50 -0400

    add test2doc fork

--- server/service/apidoc_test.go
+++ server/service/apidoc_test.go
@@ -19,6 +19,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type handlerInfo struct {
+	title       string
+	path        string
+	description string
+	method      string
+}
+
+var hinfos = map[string]handlerInfo{
+	"Login": {
+		path:        "/api/v1/kolide/login",
+		method:      "POST",
+		description: "Logs a user in.",
+		title:       "Login",
+	},
+	"Logout": {
+		path:        "/api/v1/kolide/logout",
+		method:      "POST",
+		description: "Logs a user out.",
+		title:       "Logout",
+	},
+	"Forgot Password": {
+		path:        "/api/v1/kolide/forgot_password",
+		method:      "POST",
+		description: "Sends a password reset email.",
+		title:       "Forgot Password",
+	},
+	"Me": {
+		path:        "/api/v1/kolide/me",
+		method:      "GET",
+		description: "Returns info about the currently logged in user",
+		title:       "Current Session User",
+	},
+	"ListUsers": {
+		path:        "/api/v1/kolide/users",
+		method:      "GET",
+		description: "Return a list of users",
+		title:       "List Users",
+	},
+}
+
 func TestAPIDoc(t *testing.T) {
 	ds, err := datastore.New("inmem", "")
 	assert.Nil(t, err)
@@ -30,48 +70,66 @@ func TestAPIDoc(t *testing.T) {
 	server, err := test.NewServer(handler)
 	assert.Nil(t, err)
 	defer server.Finish()
-	admin1JWT := loggedInSessionToken(t, handler)
 
 	var docTests = []struct {
-		path         string
-		method       string
+		info         handlerInfo
 		body         io.Reader
 		sessionToken string
 	}{
 		{
-			path:   "/api/v1/kolide/login",
-			method: "POST",
+			info: hinfos["Login"],
 			body: newBody([]byte(`{
         		"username": "admin1",
         		"password": "foobar"
 			}`)),
 		},
 		{
-			path:   "/api/v1/kolide/login",
-			method: "POST",
+			info: hinfos["Login"],
 			body: newBody([]byte(`{
         		"username": "nobody",
         		"password": "foobar"
 			}`)),
 		},
 		{
-			path:         "/api/v1/kolide/logout",
-			method:       "POST",
-			sessionToken: admin1JWT,
+			info:         hinfos["Logout"],
+			sessionToken: loggedInSessionToken(t, handler),
+		},
+		{
+			info: hinfos["Forgot Password"],
+			body: newBody([]byte(`{"email":"nobody@kolide.co"}`)),
+		},
+		/*{
+			path:   "/api/v1/kolide/forgot_password",
+			method: "POST",
+			body:   newBody([]byte(`{"email":"admin1@example.com"}`)),
+		},
+		TODO fails for now because mailService is nil
+		*/
+		{
+			info:         hinfos["Me"],
+			sessionToken: loggedInSessionToken(t, handler),
+		},
+		{
+			info:         hinfos["ListUsers"],
+			sessionToken: loggedInSessionToken(t, handler),
 		},
 	}
 
 	client := http.DefaultClient
 	for _, tt := range docTests {
-		req := httptest.NewRequest(tt.method, tt.path, tt.body)
-		if tt.sessionToken != "" {
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", tt.sessionToken))
-		}
-		req.RequestURI = ""
-		req.URL, err = url.Parse(fmt.Sprintf("%s%s", server.URL, req.URL.Path))
-		require.Nil(t, err)
-		_, err = client.Do(req)
-		require.Nil(t, err)
+		t.Run(tt.info.title, func(t *testing.T) {
+			req := httptest.NewRequest(tt.info.method, tt.info.path, tt.body)
+			if tt.sessionToken != "" {
+				req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", tt.sessionToken))
+			}
+			req.Header.Add("X-Test2Doc-Description", tt.info.description)
+			req.Header.Add("X-Test2Doc-Title", tt.info.title)
+			req.RequestURI = ""
+			req.URL, err = url.Parse(fmt.Sprintf("%s%s", server.URL, req.URL.Path))
+			require.Nil(t, err)
+			_, err = client.Do(req)
+			require.Nil(t, err)
+		})
 	}
 }
 
