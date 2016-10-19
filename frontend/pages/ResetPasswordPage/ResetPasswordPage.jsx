75c7d4d6f9ab8786e56bdfc0f250753fe7991003
Zachary Wasserman <zachwass2000@gmail.com>
2016-10-19 16:07:50 -0400

    Fix bugs with password resets
    - Permissions error with admin forced password reset
    - Redirecting on successful admin forced password reset
    - URL fix for forgot password reset

--- frontend/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ frontend/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -6,7 +6,7 @@ import debounce from '../../utilities/debounce';
 import { resetPassword } from '../../redux/nodes/components/ResetPasswordPage/actions';
 import ResetPasswordForm from '../../components/forms/ResetPasswordForm';
 import StackedWhiteBoxes from '../../components/StackedWhiteBoxes';
-import userActions from '../../redux/nodes/entities/users/actions';
+import { updateUser } from '../../redux/nodes/auth/actions';
 
 export class ResetPasswordPage extends Component {
   static propTypes = {
@@ -48,7 +48,7 @@ export class ResetPasswordPage extends Component {
     const { new_password: password } = formData;
     const passwordUpdateParams = { password };
 
-    return dispatch(userActions.update(user, passwordUpdateParams))
+    return dispatch(updateUser(user, passwordUpdateParams))
       .then(() => { return dispatch(push('/')); });
   }
 
