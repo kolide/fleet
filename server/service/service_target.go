b1f6c2535b4f3c4bb3bb624f7f86dde98ed97f18
Mike Arpaia <mike@arpaia.co>
2016-10-24 14:06:35 -0400

    returning pointers instead of values

--- server/service/service_target.go
+++ server/service/service_target.go
@@ -6,8 +6,8 @@ import (
     "github.com/kolide/kolide-ose/server/kolide"
 )
 
-func (svc service) SearchTargets(ctx context.Context, query string, omit []kolide.Target) (kolide.TargetSearchResults, uint, error) {
-    return kolide.TargetSearchResults{}, 0, nil
+func (svc service) SearchTargets(ctx context.Context, query string, omit []kolide.Target) (*kolide.TargetSearchResults, uint, error) {
+    return nil, 0, nil
 }
 
 func (svc service) CountHostsInTargets(ctx context.Context, targets []kolide.Target) (uint, error) {
