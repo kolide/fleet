a0e76acb356682d4af36eaad8f742dfc3b40d85f
Mike Stone <mikestone14@gmail.com>
2016-10-26 14:48:32 -0400

    TargetInfoModal for labels

--- frontend/components/modals/TargetInfoModal/TargetInfoModal.jsx
+++ frontend/components/modals/TargetInfoModal/TargetInfoModal.jsx
@@ -1,13 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import AceEditor from 'react-ace';
 import classnames from 'classnames';
-import { noop } from 'lodash';
 
 import Button from '../../buttons/Button';
 import { headerClassName } from './helpers';
 import hostHelpers from '../../hosts/HostDetails/helpers';
 import Modal from '../Modal';
 import ShadowBox from '../../ShadowBox';
+import ShadowBoxInput from '../../forms/fields/ShadowBoxInput';
 import targetInterface from '../../../interfaces/target';
 
 const baseClass = 'target-info-modal';
@@ -20,6 +20,17 @@ class TargetInfoModal extends Component {
     target: targetInterface.isRequired,
   };
 
+  renderButtons = () => {
+    const { onAdd, onExit } = this.props;
+
+    return (
+      <div className={`${baseClass}__btn-wrapper`}>
+        <Button className={`${baseClass}__cancel-btn`} text="CANCEL" variant="inverse" onClick={onExit} />
+        <Button className={`${baseClass}__add-btn`} text="ADD TO TARGETS" onClick={onAdd} />
+      </div>
+    );
+  }
+
   renderHeader = () => {
     const { target } = this.props;
     const { label } = target;
@@ -30,39 +41,38 @@ class TargetInfoModal extends Component {
         <i className={className} />
         <span>{label}</span>
       </span>
-    )
+    );
   }
 
   renderHostModal = () => {
-    const { className, onAdd, onExit, target } = this.props;
+    const { className, onExit, target } = this.props;
     const hostBaseClass = `${baseClass}__host`;
     const {
       ip,
-      label,
       mac,
       memory,
       platform,
-      os_version,
-      osquery_version,
+      os_version: osVersion,
+      osquery_version: osqueryVersion,
       status,
     } = target;
     const isOnline = status === 'online';
     const isOffline = status === 'offline';
-    const { renderHeader } = this;
+    const { renderButtons, renderHeader } = this;
     const statusClassName = classnames(`${hostBaseClass}__status`, {
-      'is-online': isOnline,
-      'is-offline': isOffline,
+      '--is-online': isOnline,
+      '--is-offline': isOffline,
     });
 
     return (
       <Modal
-        className={className}
+        className={`${className} host-modal`}
         onExit={onExit}
         title={renderHeader()}
       >
         <p className={statusClassName}>{status}</p>
         <ShadowBox>
-          <table className={`${hostBaseClass}__table`}>
+          <table className={`${baseClass}__table`}>
             <tbody>
               <tr>
                 <th>IP Address</th>
@@ -81,11 +91,11 @@ class TargetInfoModal extends Component {
               </tr>
               <tr>
                 <th>Operating System</th>
-                <td>{os_version}</td>
+                <td>{osVersion}</td>
               </tr>
               <tr>
                 <th>Osquery Version</th>
-                <td>{osquery_version}</td>
+                <td>{osqueryVersion}</td>
               </tr>
               <tr>
                 <th>Memory</th>
@@ -100,47 +110,81 @@ class TargetInfoModal extends Component {
             <span>Labels</span>
           </div>
         </div>
-        <div className={`${baseClass}__btn-wrapper`}>
-          <Button text="CANCEL" variant="inverse" onClick={onExit} />
-          <Button text="ADD TO TARGETS" onClick={onAdd} />
-        </div>
+        {renderButtons()}
       </Modal>
     );
   }
 
   renderLabelModal = () => {
-    const { className, onAdd, onExit, target } = this.props;
+    const { className, onExit, target } = this.props;
     const {
       description,
-      hosts,
-      label,
+      hosts = [],
       query,
     } = target;
-    const { renderHeader } = this;
+    const labelBaseClass = `${baseClass}__label`;
+    const { renderButtons, renderHeader } = this;
 
     return (
       <Modal
-        className={className}
+        className={`${className} label-modal`}
         onExit={onExit}
         title={renderHeader()}
       >
-        <p>{description}</p>
-        <div className={`${baseClass}__text-editor-wrapper`}>
+        <p className={`${labelBaseClass}__description`}>{description}</p>
+        <div className={`${labelBaseClass}__text-editor-wrapper`}>
           <AceEditor
             editorProps={{ $blockScrolling: Infinity }}
             mode="kolide"
             minLines={4}
             maxLines={4}
             name="modal-label-query"
-            onChange={noop}
-            showGutter
+            readOnly
+            setOptions={{ wrap: true }}
+            showGutter={false}
             showPrintMargin={false}
             theme="kolide"
             value={query}
             width="100%"
           />
         </div>
-        <Button text="ADD" onClick={onAdd} />
+        <div className={`${labelBaseClass}__search-section`}>
+          <ShadowBoxInput
+            iconClass="kolidecon-search"
+            name="search-hosts"
+            placeholder="SEARCH HOSTS"
+          />
+          <div className={`${labelBaseClass}__num-hosts-section`}>
+            <span className="num-hosts">{hosts.length} HOSTS</span>
+          </div>
+        </div>
+        <ShadowBox>
+          <table className={`${baseClass}__table`}>
+            <thead>
+              <tr>
+                <th>Hostname</th>
+                <th>Status</th>
+                <th>Platform</th>
+                <th>Location</th>
+                <th>MAC</th>
+              </tr>
+            </thead>
+            <tbody>
+              {hosts.map((host) => {
+                return (
+                  <tr className="__label-row" key={`host-${host.id}`}>
+                    <td>{host.hostname}</td>
+                    <td>{host.status}</td>
+                    <td><i className={hostHelpers.platformIconClass(host.platform)} /></td>
+                    <td>{host.ip}</td>
+                    <td>{host.mac}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </ShadowBox>
+        {renderButtons()}
       </Modal>
     );
   }
