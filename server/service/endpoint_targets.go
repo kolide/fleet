2913f5b709ac221442c13291f226bc7b54845e7e
Mike Arpaia <mike@arpaia.co>
2016-10-31 14:53:54 -0400

    initial implementation of the endpoint

--- server/service/endpoint_targets.go
+++ server/service/endpoint_targets.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"errors"
-
 	"github.com/go-kit/kit/endpoint"
 	"github.com/kolide/kolide-ose/server/kolide"
 	"golang.org/x/net/context"
@@ -23,6 +21,7 @@ type targetsData struct {
 
 type searchTargetsResponse struct {
 	Targets *targetsData `json:"targets,omitempty"`
+	Count   uint         `json:"count,omitempty"`
 	Err     error        `json:"error,omitempty"`
 }
 
@@ -30,6 +29,29 @@ func (r searchTargetsResponse) error() error { return r.Err }
 
 func makeSearchTargetsEndpoint(svc kolide.Service) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		return nil, errors.New("Unimplemented")
+		req := request.(searchTargetsRequest)
+		// TODO: add the omit value
+		results, count, err := svc.SearchTargets(ctx, req.Query, nil)
+		if err != nil {
+			return searchTargetsResponse{Err: err}, nil
+		}
+
+		targets := &targetsData{
+			Hosts:  []hostResponse{},
+			Labels: []kolide.Label{},
+		}
+
+		for _, host := range results.Hosts {
+			targets.Hosts = append(targets.Hosts, hostResponse{host, svc.HostStatus(ctx, host)})
+		}
+
+		for _, label := range results.Labels {
+			targets.Labels = append(targets.Labels, label)
+		}
+
+		return searchTargetsResponse{
+			Targets: targets,
+			Count:   count,
+		}, nil
 	}
 }
