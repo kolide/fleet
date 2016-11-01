8d93023b44b97f82aaab55ea98821f391ae7e2c4
Mike Arpaia <mike@arpaia.co>
2016-11-01 10:23:35 -0400

    comment on what SearchTargets is supposed to do

--- server/kolide/target.go
+++ server/kolide/target.go
@@ -10,7 +10,13 @@ type TargetSearchResults struct {
 }
 
 type TargetService interface {
+	// SearchTargets will accept a search query, a slice of IDs of hosts to omit,
+	// and a slice of IDs of labels to omit, and it will return a set of targets
+	// (hosts and label) which match the supplied search query. It will also return
+	// a uint which represents the number of unique hosts which are currently
+	// selected, as indicated by selectedHostIDs and selectedLabelIDs
 	SearchTargets(ctx context.Context, query string, selectedHostIDs []uint, selectedLabelIDs []uint) (*TargetSearchResults, uint, error)
+
 	CountHostsInTargets(ctx context.Context, hosts []Host, labels []Label) (uint, error)
 }
 
