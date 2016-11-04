4745ceebcd0b962823de7380ad063746440db308
Mike Arpaia <mike@arpaia.co>
2016-11-04 16:13:30 -0400

    Add description attribute to packs

--- frontend/interfaces/pack.js
+++ frontend/interfaces/pack.js
@@ -3,6 +3,7 @@ import { PropTypes } from 'react';
 export default PropTypes.shape({
   detail_updated_at: PropTypes.string,
   name: PropTypes.string,
+  description: PropTypes.string,
   id: PropTypes.number,
   platform: PropTypes.string,
   updated_at: PropTypes.string,
