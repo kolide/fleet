81930c00b33085f53e5c325c4b9d6689a937713d
Mike Arpaia <mike@arpaia.co>
2016-11-04 14:41:00 -0400

    pack interface

---
+++ frontend/interfaces/pack.js
@@ -0,0 +1,9 @@
+import { PropTypes } from 'react';
+
+export default PropTypes.shape({
+  detail_updated_at: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.number,
+  platform: PropTypes.string,
+  updated_at: PropTypes.string,
+});
