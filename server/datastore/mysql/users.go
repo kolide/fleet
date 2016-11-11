bb0f2be42c8c6e8a7817b46b875eddfd22569320
John Murphy <murphybytes@gmail.com>
2016-11-11 15:33:43 +0800

    Added mysql user entity

--- server/datastore/mysql/users.go
+++ server/datastore/mysql/users.go
@@ -1,28 +1,111 @@
 package mysql
 
 import (
+	"fmt"
+
+	"github.com/kolide/kolide-ose/server/errors"
 	"github.com/kolide/kolide-ose/server/kolide"
 )
 
+// NewUser creates a new user
 func (d *Datastore) NewUser(user *kolide.User) (*kolide.User, error) {
-	panic("not implemented")
-}
-func (d *Datastore) User(username string) (*kolide.User, error) {
-	panic("not implemented")
+	sqlStatement := `
+		INSERT INTO users (
+			created_at,
+			updated_at,
+			password,
+			salt,
+			name,
+			email,
+			admin,
+			enabled,
+			admin_forced_password_reset,
+			gravatar_url,
+			position
+		) VALUES (?,?,?,?,?,?,?,?,?,?,?)
+	`
+	user.MarkAsCreated(d.clock.Now())
+	result, err := d.db.Exec(sqlStatement, user.CreatedAt, user.UpdatedAt,
+		user.Password, user.Salt, user.Name, user.Email, user.Admin, user.Enabled,
+		user.AdminForcedPasswordReset, user.GravatarURL, user.Position)
+
+	if err != nil {
+		return nil, errors.DatabaseError(err)
+	}
+
+	id, _ := result.LastInsertId()
+	user.ID = uint(id)
+	return user, nil
 }
 
+func (d *Datastore) findUser(searchCol string, searchVal interface{}) (*kolide.User, error) {
+	sqlStatement := fmt.Sprintf(
+		"SELECT * FROM users "+
+			"WHERE %s = ? AND NOT deleted LIMIT 1",
+		searchCol,
+	)
+
+	user := &kolide.User{}
+
+	if err := d.db.Get(user, sqlStatement, searchVal); err != nil {
+		return nil, errors.DatabaseError(err)
+	}
+
+	return user, nil
+}
+
+// User retrieves a user by name
+func (d *Datastore) User(username string) (*kolide.User, error) {
+	return d.findUser("username", username)
+}
+
+// ListUsers lists all users with limit, sort and offset passed in with
+// kolide.ListOptions
 func (d *Datastore) ListUsers(opt kolide.ListOptions) ([]*kolide.User, error) {
-	panic("not implemented")
+	sqlStatement := `
+		SELECT * FROM USERS WHERE NOT deleted
+	`
+	sqlStatement = appendListOptionsToSQL(sqlStatement, opt)
+	users := []*kolide.User{}
+
+	if err := d.db.Select(&users, sqlStatement); err != nil {
+		return nil, errors.DatabaseError(err)
+	}
+
+	return users, nil
+
 }
 
 func (d *Datastore) UserByEmail(email string) (*kolide.User, error) {
-	panic("not implemented")
+	return d.findUser("email", email)
 }
 
 func (d *Datastore) UserByID(id uint) (*kolide.User, error) {
-	panic("not implemented")
+	return d.findUser("id", id)
 }
 
 func (d *Datastore) SaveUser(user *kolide.User) error {
-	panic("not implemented")
+	user.MarkAsUpdated(d.clock.Now())
+	sqlStatement := `
+		UPDATE user SET
+			updated_at = ?,
+			username = ?,
+			password = ?,
+			salt = ?,
+			name = ?,
+			email = ?,
+			admin = ?,
+			enabled = ?.
+			admin_forced_password_reset = ?,
+			gravatar_url = ?,
+			postion = ?
+		WHERE id = ?
+	`
+	_, err := d.db.Exec(sqlStatement, user.UpdatedAt, user.Username, user.Password,
+		user.Salt, user.Name, user.Email, user.Admin, user.Enabled,
+		user.AdminForcedPasswordReset, user.GravatarURL, user.Position, user.ID)
+	if err != nil {
+		return errors.DatabaseError(err)
+	}
+	return nil
 }
