b94d2266b47e94d2380c6967aa4ed009cbcfa813
John Murphy <murphybytes@gmail.com>
2016-11-15 22:01:30 +0800

    Removed extraneous file

--- tmp/tsreq/frontend/components/forms/validators/valid_email/valid_email.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var EMAIL_REGEX = /\S+@\S+\.\S+/;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = function (email) {
-    if (EMAIL_REGEX.test(email)) {
-        return true;
-    }
-    return false;
-};
