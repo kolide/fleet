1e42eb2e31f85057102117fd0f151178e6b8e013
Kyle Knight <kyle@kolide.co>
2016-11-17 14:12:32 -0600

    Handle changing the switch

--- frontend/components/buttons/Rocker/Rocker.jsx
+++ frontend/components/buttons/Rocker/Rocker.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
+import { noop } from 'lodash';
 import classnames from 'classnames';
 
 class Rocker extends Component {
 
   static propTypes = {
     className: PropTypes.string,
+    handleChange: PropTypes.func,
     name: PropTypes.string,
     options: PropTypes.shape({
       aText: PropTypes.string,
@@ -15,8 +17,12 @@ class Rocker extends Component {
     value: PropTypes.string,
   };
 
+  static defaultProps = {
+    handleChange: noop,
+  };
+
   render () {
-    const { className, name, options, value } = this.props;
+    const { className, handleChange, name, options, value } = this.props;
     const { aText, aIcon, bText, bIcon } = options;
     const baseClass = 'kolide-rocker';
 
@@ -25,7 +31,7 @@ class Rocker extends Component {
     return (
       <div className={rockerClasses}>
         <label className={`${baseClass}__label`} htmlFor={name}>
-          <input className={`${baseClass}__checkbox`} type="checkbox" value={value} name={name} />
+          <input className={`${baseClass}__checkbox`} type="checkbox" value={value} name={name} onChange={handleChange} />
           <span className={`${baseClass}__switch ${baseClass}__switch--opt-b`}>
             <span className={`${baseClass}__text`}>
               <i className={`kolidecon kolidecon-${bIcon}`} /> {bText}
