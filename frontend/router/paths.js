aac8a8c8d8c877143cc8efc40857393235de1e0a
Mike Arpaia <mike@arpaia.co>
2016-11-18 18:49:10 -0800

    alpha order in paths.js

--- frontend/router/paths.js
+++ frontend/router/paths.js
@@ -1,10 +1,10 @@
 export default {
   ADMIN_DASHBOARD: '/admin',
+  ALL_PACKS: '/packs/all',
   FORGOT_PASSWORD: '/login/forgot',
   HOME: '/',
   LOGIN: '/login',
   LOGOUT: '/logout',
-  RESET_PASSWORD: '/login/reset',
-  ALL_PACKS: '/packs/all',
   NEW_PACK: '/packs/new',
+  RESET_PASSWORD: '/login/reset',
 };
