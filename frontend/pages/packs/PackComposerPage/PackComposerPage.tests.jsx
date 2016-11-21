8dec8d7fe036609bcd23c7eb4a2c6291b6eaf2b5
Mike Stone <mikestone14@gmail.com>
2016-11-21 18:17:14 -0500

    Adds Checkbox component

--- frontend/pages/packs/PackComposerPage/PackComposerPage.tests.jsx
+++ frontend/pages/packs/PackComposerPage/PackComposerPage.tests.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
 
-import { PackComposerPage } from './PackComposerPage';
+import { connectedComponent, reduxMockStore } from 'test/helpers';
+import ConnectedPacksComposerPage, { PackComposerPage } from './PackComposerPage';
 
 describe('PackComposerPage - component', () => {
   it('renders', () => {
@@ -15,5 +16,15 @@ describe('PackComposerPage - component', () => {
     const page = mount(<PackComposerPage />);
 
     expect(page.find('PackForm').length).toEqual(1);
+  });
+
+  it('loads all queries when it mounts', () => {
+    const mockStore = reduxMockStore();
+    const page = mount(
+      connectedComponent(ConnectedPacksComposerPage, { mockStore })
+    );
+
+    expect(page.length).toEqual(1);
+    expect(mockStore.getActions()).toInclude({ type: 'queries_LOAD_REQUEST' });
   });
 });
