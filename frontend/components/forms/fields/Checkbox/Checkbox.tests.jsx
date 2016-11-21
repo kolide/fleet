987135810fc1dfabcc255cd0b4f9833a167d4f37
Mike Stone <mikestone14@gmail.com>
2016-11-21 18:17:14 -0500

    Adds Checkbox component

--- frontend/components/forms/fields/Checkbox/Checkbox.tests.jsx
+++ frontend/components/forms/fields/Checkbox/Checkbox.tests.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import expect from 'expect';
+import expect, { createSpy, restoreSpies } from 'expect';
 import { mount } from 'enzyme';
 
 import Checkbox from './Checkbox';
 
 describe('Checkbox - component', () => {
+  afterEach(restoreSpies);
+
   it('renders', () => {
     expect(mount(<Checkbox />)).toExist();
+  });
+
+  it('calls the "onChange" handler when changed', () => {
+    const onCheckedComponentChangeSpy = createSpy();
+    const onUncheckedComponentChangeSpy = createSpy();
+
+    const checkedComponent = mount(
+      <Checkbox
+        checked
+        name="checkbox"
+        onChange={onCheckedComponentChangeSpy}
+      />
+    ).find('input');
+
+    const uncheckedComponent = mount(
+      <Checkbox
+        checked={false}
+        name="checkbox"
+        onChange={onUncheckedComponentChangeSpy}
+      />
+    ).find('input');
+
+    checkedComponent.simulate('change');
+    uncheckedComponent.simulate('change');
+
+    expect(onCheckedComponentChangeSpy).toHaveBeenCalledWith(false);
+    expect(onUncheckedComponentChangeSpy).toHaveBeenCalledWith(true);
   });
 });
