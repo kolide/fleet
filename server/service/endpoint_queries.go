97f252ee9384c33b6c1adf79cd1636d7ce7b6339
Zachary Wasserman <zachwass2000@gmail.com>
2016-11-22 16:11:43 -0800

    fix import after rebase

--- server/service/endpoint_queries.go
+++ server/service/endpoint_queries.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-kit/kit/endpoint"
+	"github.com/kolide/kolide-ose/server/contexts/viewer"
 	"github.com/kolide/kolide-ose/server/kolide"
 	"github.com/kolide/kolide-ose/server/websocket"
 	"golang.org/x/net/context"
