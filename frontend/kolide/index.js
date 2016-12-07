23b3e53ee2061a6c42ecbb63b64268856bbada84
Mike Stone <mikestone14@gmail.com>
2016-12-07 16:25:04 -0500

    Makes items routable on the Manage Hosts Sidebar

--- frontend/kolide/index.js
+++ frontend/kolide/index.js
@@ -124,18 +124,23 @@ class Kolide extends Base {
 
     return this.authenticatedGet(this.endpoint(LABELS))
       .then((response) => {
+        const labelTypeForDisplayText = {
+          'All Hosts': 'all',
+          'MS Windows': 'platform',
+          'CentOS Linux': 'platform',
+          'Mac OS X': 'platform',
+          'Ubuntu Linux': 'platform',
+        };
         const labels = response.labels.map((label) => {
-          return { ...label, type: 'custom' };
+          return {
+            ...label,
+            type: labelTypeForDisplayText[label.display_text] || 'custom',
+          };
         });
         const stubbedLabels = [
-          { id: 100, display_text: 'All Hosts', type: 'all', count: 22 },
           { id: 40, display_text: 'ONLINE', type: 'status', count: 20 },
           { id: 50, display_text: 'OFFLINE', type: 'status', count: 2 },
           { id: 55, display_text: 'MIA', description: '(offline > 30 days)', type: 'status', count: 3 },
-          { id: 60, display_text: 'macOS', type: 'platform', count: 1 },
-          { id: 70, display_text: 'Windows', type: 'platform', count: 1 },
-          { id: 80, display_text: 'Ubuntu', type: 'platform', count: 10 },
-          { id: 90, display_text: 'Centos', type: 'platform', count: 10 },
         ];
 
         return labels.concat(stubbedLabels);
