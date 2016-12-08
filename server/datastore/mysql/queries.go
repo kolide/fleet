1d00c7fe2cc75480c9ad3c75ece2650b3b0f1a0f
Zachary Wasserman <zachwass2000@gmail.com>
2016-12-08 10:16:11 -0800

    Add service method for bulk deleting queries
    - New datastore method for bulk deletion
    - New service method calling this datastore method
    - Endpoint, transport and handler connections for service method
    
    Closes #389

--- server/datastore/mysql/queries.go
+++ server/datastore/mysql/queries.go
@@ -1,6 +1,8 @@
 package mysql
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/kolide/kolide-ose/server/errors"
 	"github.com/kolide/kolide-ose/server/kolide"
@@ -53,15 +55,41 @@ func (d *Datastore) DeleteQuery(query *kolide.Query) error {
 	query.MarkDeleted(d.clock.Now())
 	sql := `
 		UPDATE queries
-			SET deleted_at = ?, deleted = ?
+			SET deleted_at = ?, deleted = true
 			WHERE id = ?
 	`
-	_, err := d.db.Exec(sql, query.DeletedAt, true, query.ID)
+	_, err := d.db.Exec(sql, query.DeletedAt, query.ID)
 	if err != nil {
 		return errors.DatabaseError(err)
 	}
 
 	return nil
+}
+
+// DeleteQueries (soft) deletes the existing query objects with the provided
+// IDs. The number of deleted queries is returned along with any error.
+func (d *Datastore) DeleteQueries(ids []uint) (uint, error) {
+	sql := `
+		UPDATE queries
+			SET deleted_at = NOW(), deleted = true
+			WHERE id IN (?)
+	`
+	query, args, err := sqlx.In(sql, ids)
+	if err != nil {
+		return 0, errors.DatabaseError(err)
+	}
+
+	result, err := d.db.Exec(query, args...)
+	if err != nil {
+		return 0, errors.DatabaseError(err)
+	}
+
+	deleted, err := result.RowsAffected()
+	if err != nil {
+		return 0, errors.DatabaseError(err)
+	}
+
+	return uint(deleted), nil
 }
 
 // Query returns a single Query identified by id, if such
@@ -115,6 +143,10 @@ func (d *Datastore) ListQueries(opt kolide.ListOptions) ([]*kolide.Query, error)
 
 // loadPacksForQueries loads the packs associated with the provided queries
 func (d *Datastore) loadPacksForQueries(queries []*kolide.Query) error {
+	if len(queries) == 0 {
+		return nil
+	}
+
 	sql := `
 		SELECT p.*, pq.query_id AS query_id
 		FROM packs p
@@ -135,6 +167,7 @@ func (d *Datastore) loadPacksForQueries(queries []*kolide.Query) error {
 
 	query, args, err := sqlx.In(sql, ids)
 	if err != nil {
+		fmt.Println("in", err)
 		return errors.DatabaseError(err)
 	}
 
@@ -145,6 +178,7 @@ func (d *Datastore) loadPacksForQueries(queries []*kolide.Query) error {
 
 	err = d.db.Select(&rows, query, args...)
 	if err != nil {
+		fmt.Println("select", err)
 		return errors.DatabaseError(err)
 	}
 
