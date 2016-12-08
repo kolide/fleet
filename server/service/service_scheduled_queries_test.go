ad6377cd681a1640c865d0d88f145f1ed3b747ce
Mike Arpaia <mike@arpaia.co>
2016-12-07 17:31:16 -0800

    test outline

---
+++ server/service/service_scheduled_queries_test.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"testing"
+)
+
+func TestScheduleAndGetScheduledQuery(t *testing.T) {
+}
+
+func TestGetScheduledQuery(t *testing.T) {
+}
+
+func TestModifyScheduledQuery(t *testing.T) {
+}
+
+func TestDeleteScheduledQuery(t *testing.T) {
+}
