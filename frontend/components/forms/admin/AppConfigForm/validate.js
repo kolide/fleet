ad70061e8851327a9cf4ce0f026b225f65e835e0
Mike Stone <mikestone14@gmail.com>
2016-12-22 16:30:04 -0500

    AppConfigForm client side validation

--- frontend/components/forms/admin/AppConfigForm/validate.js
+++ frontend/components/forms/admin/AppConfigForm/validate.js
@@ -1,3 +1,36 @@
-export default () => {
-  return { valid: true, errors: {} };
+import { size } from 'lodash';
+
+export default (formData) => {
+  const errors = {};
+  const {
+    kolide_server_url: kolideServerUrl,
+    smtp_sender_address: smtpSenderAddress,
+    server: smtpServer,
+    user_name: smtpUserName,
+    password: smtpPassword,
+  } = formData;
+
+  if (!kolideServerUrl) {
+    errors.kolide_server_url = 'Kolide Server URL must be present';
+  }
+
+  if (!smtpSenderAddress) {
+    errors.smtp_sender_address = 'SMTP Sender Address must be present';
+  }
+
+  if (!smtpServer) {
+    errors.server = 'SMTP Server must be present';
+  }
+
+  if (!smtpUserName) {
+    errors.user_name = 'SMTP Username must be present';
+  }
+
+  if (!smtpPassword) {
+    errors.password = 'SMTP Password must be present';
+  }
+
+  const valid = !size(errors);
+
+  return { valid, errors };
 };
