kolide/fleet

commit 8567cc458c0cc5399d65f020782d521a4ceeb636

Run query (#549)
* Adds campaigns to redux state

* Update campaign with web socket data

* Destroy the current campaign when creating a new one

* close the socket when leaving the page or creating a new campaign

* Allow stopping a running query

* Update campaign with query results

* Adds QueryResultsTable

* Display flash message if campaign can't be created

* Allow filtering query results

* Adds filter icon

* Prevent query text updates when the query is running

frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
@@ -84,7 +84,7 @@ class UserManagementPage extends Component {
               return dispatch(renderFlash('success', 'User forced to reset password', update(user, { force_password_reset: false })));
             });
         case 'revert_invitation':
-          return dispatch(inviteActions.destroy({ entityID: user.id }))
+          return dispatch(inviteActions.destroy(user))
             .then(() => {
               return dispatch(renderFlash('success', 'Invite revoked'));
             });

frontend/components/forms/fields/InputField/InputField.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
-import { pick } from 'lodash';
+import { noop, pick } from 'lodash';
 
 import FormField from 'components/forms/FormField';
 
@@ -18,6 +18,7 @@ class InputField extends Component {
     labelClassName: PropTypes.string,
     name: PropTypes.string,
     onChange: PropTypes.func,
+    onFocus: PropTypes.func,
     placeholder: PropTypes.string,
     type: PropTypes.string,
     value: PropTypes.string.isRequired,
@@ -29,6 +30,7 @@ class InputField extends Component {
     inputOptions: {},
     label: null,
     labelClassName: '',
+    onFocus: noop,
     type: 'text',
     value: '',
   };
@@ -54,7 +56,17 @@ class InputField extends Component {
   }
 
   render () {
-    const { error, inputClassName, inputOptions, inputWrapperClass, name, placeholder, type, value } = this.props;
+    const {
+      error,
+      inputClassName,
+      inputOptions,
+      inputWrapperClass,
+      name,
+      onFocus,
+      placeholder,
+      type,
+      value,
+    } = this.props;
     const { onInputChange } = this;
     const shouldShowPasswordClass = type === 'password';
     const inputClasses = classnames(baseClass, inputClassName, {
@@ -87,6 +99,7 @@ class InputField extends Component {
         <input
           name={name}
           onChange={onInputChange}
+          onFocus={onFocus}
           className={inputClasses}
           placeholder={placeholder}
           ref={(r) => { this.input = r; }}

frontend/components/forms/fields/SelectTargetsDropdown/SelectTargetsDropdown.jsx
@@ -4,7 +4,7 @@ import classnames from 'classnames';
 
 import Kolide from 'kolide';
 import targetInterface from 'interfaces/target';
-import { formatSelectedTargetsForApi } from './helpers';
+import { formatSelectedTargetsForApi } from 'kolide/helpers';
 import Input from './SelectTargetsInput';
 import Menu from './SelectTargetsMenu';
 

frontend/components/forms/fields/SelectTargetsDropdown/helpers.js
@@ -1,17 +1,0 @@
-import { flatMap } from 'lodash';
-
-const filterTarget = (targetType) => {
-  return (target) => {
-    return target.target_type === targetType ? [target.id] : [];
-  };
-};
-
-export const formatSelectedTargetsForApi = (selectedTargets) => {
-  const targets = selectedTargets || [];
-  const hosts = flatMap(targets, filterTarget('hosts'));
-  const labels = flatMap(targets, filterTarget('labels'));
-
-  return { hosts, labels };
-};
-
-export default { formatSelectedTargetsForApi };

frontend/components/forms/fields/SelectTargetsDropdown/helpers.tests.js
@@ -1,23 +1,0 @@
-import expect from 'expect';
-
-import helpers from './helpers';
-
-const label1 = { id: 1, target_type: 'labels' };
-const label2 = { id: 2, target_type: 'labels' };
-const host1 = { id: 6, target_type: 'hosts' };
-const host2 = { id: 5, target_type: 'hosts' };
-
-describe('SelectTargetsDropdown - helpers', () => {
-  describe('#formatSelectedTargetsForApi', () => {
-    const { formatSelectedTargetsForApi } = helpers;
-
-    it('splits targets into labels and hosts', () => {
-      const targets = [host1, host2, label1, label2];
-
-      expect(formatSelectedTargetsForApi(targets)).toEqual({
-        hosts: [6, 5],
-        labels: [1, 2],
-      });
-    });
-  });
-});

frontend/components/forms/queries/QueryForm/QueryForm.jsx
@@ -15,8 +15,10 @@ class QueryForm extends Component {
     onCancel: PropTypes.func,
     onRunQuery: PropTypes.func,
     onSave: PropTypes.func,
+    onStopQuery: PropTypes.func,
     onUpdate: PropTypes.func,
     query: queryInterface,
+    queryIsRunning: PropTypes.bool,
     queryText: PropTypes.string.isRequired,
     queryType: PropTypes.string,
   };
@@ -163,8 +165,35 @@ class QueryForm extends Component {
   renderButtons = () => {
     const { canSaveAsNew, canSaveChanges } = helpers;
     const { formData } = this.state;
-    const { onRunQuery, query, queryType } = this.props;
+    const {
+      onRunQuery,
+      onStopQuery,
+      query,
+      queryIsRunning,
+      queryType,
+    } = this.props;
     const { onCancel, onSave, onUpdate } = this;
+    let runQueryButton;
+
+    if (queryIsRunning) {
+      runQueryButton = (
+        <Button
+          className={`${baseClass}__stop-query-btn`}
+          onClick={onStopQuery}
+          text="Stop Query"
+          variant="alert"
+        />
+      );
+    } else {
+      runQueryButton = (
+        <Button
+          className={`${baseClass}__run-query-btn`}
+          onClick={onRunQuery}
+          text="Run Query"
+          variant="brand"
+        />
+      );
+    }
 
     if (queryType === 'label') {
       return (
@@ -202,12 +231,7 @@ class QueryForm extends Component {
           text="Save As New..."
           variant="success"
         />
-        <Button
-          className={`${baseClass}__run-query-btn`}
-          onClick={onRunQuery}
-          text="Run Query"
-          variant="brand"
-        />
+        {runQueryButton}
       </div>
     );
   }

frontend/components/forms/queries/QueryForm/QueryForm.tests.jsx
@@ -25,6 +25,36 @@ describe('QueryForm - component', () => {
     expect(inputFields.find({ name: 'description' }).length).toEqual(1);
   });
 
+  it('renders a "stop query" button when a query is running', () => {
+    const form = mount(<QueryForm query={query} queryIsRunning queryText={queryText} />);
+    const runQueryBtn = form.find('.query-form__run-query-btn');
+    const stopQueryBtn = form.find('.query-form__stop-query-btn');
+
+    expect(runQueryBtn.length).toEqual(0);
+    expect(stopQueryBtn.length).toEqual(1);
+  });
+
+  it('renders a "run query" button when a query is not running', () => {
+    const form = mount(<QueryForm query={query} queryIsRunning={false} queryText={queryText} />);
+    const runQueryBtn = form.find('.query-form__run-query-btn');
+    const stopQueryBtn = form.find('.query-form__stop-query-btn');
+
+    expect(runQueryBtn.length).toEqual(1);
+    expect(stopQueryBtn.length).toEqual(0);
+  });
+
+  it('calls the onStopQuery prop when the stop query button is clicked', () => {
+    const onStopQuerySpy = createSpy();
+    const form = mount(
+      <QueryForm onStopQuery={onStopQuerySpy} query={query} queryIsRunning queryText={queryText} />
+    );
+    const stopQueryBtn = form.find('.query-form__stop-query-btn');
+
+    stopQueryBtn.simulate('click');
+
+    expect(onStopQuerySpy).toHaveBeenCalled();
+  });
+
   it('updates state on input field change', () => {
     const form = mount(<QueryForm query={query} queryText={queryText} />);
     const inputFields = form.find('InputField');

frontend/components/queries/QueryComposer/QueryComposer.jsx
@@ -19,10 +19,12 @@ class QueryComposer extends Component {
     onOsqueryTableSelect: PropTypes.func,
     onRunQuery: PropTypes.func,
     onSave: PropTypes.func,
+    onStopQuery: PropTypes.func,
     onTargetSelect: PropTypes.func,
     onTextEditorInputChange: PropTypes.func,
     onUpdate: PropTypes.func,
     query: queryInterface,
+    queryIsRunning: PropTypes.bool,
     queryType: PropTypes.string,
     selectedTargets: PropTypes.arrayOf(targetInterface),
     targetsCount: PropTypes.number,
@@ -56,8 +58,10 @@ class QueryComposer extends Component {
       onFormCancel,
       onRunQuery,
       onSave,
+      onStopQuery,
       onUpdate,
       query,
+      queryIsRunning,
       queryText,
       queryType,
     } = this.props;
@@ -67,8 +71,10 @@ class QueryComposer extends Component {
         onCancel={onFormCancel}
         onRunQuery={onRunQuery}
         onSave={onSave}
+        onStopQuery={onStopQuery}
         onUpdate={onUpdate}
         query={query}
+        queryIsRunning={queryIsRunning}
         queryType={queryType}
         queryText={queryText}
       />
@@ -105,7 +111,7 @@ class QueryComposer extends Component {
   }
 
   render () {
-    const { onTextEditorInputChange, queryText, queryType } = this.props;
+    const { onTextEditorInputChange, queryIsRunning, queryText, queryType } = this.props;
     const { onLoad, renderForm, renderTargetsInput } = this;
 
     return (
@@ -122,6 +128,7 @@ class QueryComposer extends Component {
             name="query-editor"
             onLoad={onLoad}
             onChange={onTextEditorInputChange}
+            readOnly={queryIsRunning}
             setOptions={{ enableLinking: true }}
             showGutter
             showPrintMargin={false}

commit c861a3d82bba7ab7c8b73ce4b02afadcffa3a389

use entityID when deleting invite
Fixes #696
Regression from 8567cc458c0cc5399d65f020782d521a4ceeb636

frontend/pages/Admin/UserManagementPage/UserManagementPage.jsx
@@ -84,7 +84,7 @@ class UserManagementPage extends Component {
               return dispatch(renderFlash('success', 'User forced to reset password', update(user, { force_password_reset: false })));
             });
         case 'revert_invitation':
-          return dispatch(inviteActions.destroy(user))
+          return dispatch(inviteActions.destroy({ entityID: user.id }))
             .then(() => {
               return dispatch(renderFlash('success', 'Invite revoked'));
             });