e2d5001de1c7ebe8e167fdbc3cbf4297b2a0e214
Zachary Wasserman <zachwass2000@gmail.com>
2016-12-23 10:25:18 -0800

    Close orphaned distributed query campaign after failed publish attempt
    A distributed query campaign can be "orphaned" (left in the QueryRunning state)
    if the Kolide server restarts while it is running, or other weirdness occurs.
    When this happens, no subscribers are waiting to read results written by
    osqueryd agents, but the agents continue to receive the query. Previously, this
    would cause us to error on ingestion.
    
    The new behavior will instead set the campaign to completed when it detects
    that it is orphaned. This should prevent sending queries for which there is no
    subscriber.
    
    - New NoSubscriber error interface in pubsub
    - Detect NoSubscriber errors and close campaigns
    - Tests on pubsub and service methods
    
    Fixes #695

--- server/pubsub/doc.go
+++ server/pubsub/doc.go
@@ -1,2 +1,24 @@
 // Package pubsub implements pub/sub interfaces defined in package kolide.
 package pubsub
+
+// NoSubscriber is an interface implemented by errors in the pubsub package
+// that allows handlers to determine whether the error is due to no subscribers
+type NoSubscriber interface {
+	// NoSubscriber returns true if the error occurred because there are no
+	// subscribers on the channel
+	NoSubscriber() bool
+}
+
+// NoSubscriberError can be returned when channel operations fail because there
+// are no subscribers. Its NoSubscriber() method always returns true.
+type NoSubscriberError struct {
+	Channel string
+}
+
+func (e NoSubscriberError) Error() string {
+	return "no subscriber for channel " + e.Channel
+}
+
+func (e NoSubscriberError) NoSubscriber() bool {
+	return true
+}
