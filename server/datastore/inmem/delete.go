01accfaf9439bc4f5c4c867cafe23d088b672679
Victor Vrantchan <vrancean+github@gmail.com>
2016-12-28 16:23:07 -0500

    add comment on inmem delete

--- server/datastore/inmem/delete.go
+++ server/datastore/inmem/delete.go
@@ -11,9 +11,13 @@ func (d *Datastore) Delete(e kolide.Entity) error {
 	d.mtx.Lock()
 	defer d.mtx.Unlock()
 
-	v := reflect.ValueOf(d).Elem().FieldByName(strings.Title(e.EntityType()))
-	if v.IsValid() {
-		v.SetMapIndex(reflect.ValueOf(e.EntityID()), reflect.Value{})
+	// use reflect to get the value of a field which matches the EntityType()
+	// for example , `invites` would get the datastore.Invites field,
+	// which is a map. Setting the map key to reflect.Value{} achieves the same
+	// result as delete(map, id) would.
+	field := reflect.ValueOf(d).Elem().FieldByName(strings.Title(e.EntityType()))
+	if field.IsValid() {
+		field.SetMapIndex(reflect.ValueOf(e.EntityID()), reflect.Value{})
 	}
 	return nil
 }
