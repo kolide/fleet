fd10f6b4930cc55e25a9c9252d5cf620116ea1eb
Victor Vrantchan <vrancean+github@gmail.com>
2016-12-29 12:37:51 -0500

    Add endpoint to retrieve an invite with the invite token.
    Closes #579

--- server/datastore/mysql/invites.go
+++ server/datastore/mysql/invites.go
@@ -47,27 +47,40 @@ func (d *Datastore) ListInvites(opt kolide.ListOptions) ([]*kolide.Invite, error
 
 // Invite returns Invite identified by id.
 func (d *Datastore) Invite(id uint) (*kolide.Invite, error) {
-	invite := &kolide.Invite{}
-	err := d.db.Get(invite, "SELECT * FROM invites WHERE id = ? AND NOT deleted", id)
+	var invite kolide.Invite
+	err := d.db.Get(&invite, "SELECT * FROM invites WHERE id = ? AND NOT deleted", id)
 	if err != nil && err == sql.ErrNoRows {
 		return nil, notFound("Invite").WithID(id)
 	} else if err != nil {
 		return nil, errors.Wrap(err, "select invite by ID")
 	}
-	return invite, nil
+	return &invite, nil
 }
 
 // InviteByEmail finds an Invite with a particular email, if one exists.
 func (d *Datastore) InviteByEmail(email string) (*kolide.Invite, error) {
-	invite := &kolide.Invite{}
-	err := d.db.Get(invite, "SELECT * FROM invites WHERE email = ? AND NOT deleted", email)
+	var invite kolide.Invite
+	err := d.db.Get(&invite, "SELECT * FROM invites WHERE email = ? AND NOT deleted", email)
 	if err != nil && err == sql.ErrNoRows {
 		return nil, notFound("Invite").
 			WithMessage(fmt.Sprintf("with email %s", email))
 	} else if err != nil {
-		return nil, errors.Wrap(err, "sqlx get invite")
+		return nil, errors.Wrap(err, "sqlx get invite by email")
 	}
-	return invite, nil
+	return &invite, nil
+}
+
+// InviteByToken finds an Invite with a particular token, if one exists.
+func (d *Datastore) InviteByToken(token string) (*kolide.Invite, error) {
+	var invite kolide.Invite
+	err := d.db.Get(&invite, "SELECT * FROM invites WHERE token = ? AND NOT deleted", token)
+	if err != nil && err == sql.ErrNoRows {
+		return nil, notFound("Invite").
+			WithMessage(fmt.Sprintf("with token %s", token))
+	} else if err != nil {
+		return nil, errors.Wrap(err, "sqlx get invite by token")
+	}
+	return &invite, nil
 }
 
 // SaveInvite modifies existing Invite
