1546ab21eddc991d6e1f23bd4b6964a34e93bb31
Kyle Knight <kyle@kolide.co>
2017-01-06 11:47:21 -0600

    404 Page

---
+++ frontend/pages/Kolide404/Kolide404.tests.jsx
@@ -0,0 +1,19 @@
+// import expect from 'expect';
+// import { mount } from 'enzyme';
+
+// import ConnectedPage from 'pages/Kolide404';
+// import testHelpers from 'test/helpers';
+// import { userStub } from 'test/stubs';
+
+// const { connectedComponent, reduxMockStore } = testHelpers;
+
+// describe('Kolide404 - component', () => {
+//   it('renders a UserSettingsForm component', () => {
+//     const store = { auth: { user: userStub } };
+//     const mockStore = reduxMockStore(store);
+
+//     const page = mount(connectedComponent(ConnectedPage, { mockStore }));
+
+//     expect(page.find('UserSettingsForm').length).toEqual(1);
+//   });
+// });
