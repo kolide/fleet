9d041f83e59f91fc39b4ad3e35460359a4ba4e88
Mike Stone <mike@gnar.dog>
2017-01-20 10:33:44 -0500

    Fix missing info in select targets dropdown

--- frontend/redux/nodes/entities/hosts/config.js
+++ frontend/redux/nodes/entities/hosts/config.js
@@ -1,8 +1,7 @@
-import { find } from 'lodash';
-
-import Kolide from '../../../../kolide';
-import reduxConfig from '../base/reduxConfig';
-import schemas from '../base/schemas';
+import Kolide from 'kolide';
+import reduxConfig from 'redux/nodes/entities/base/reduxConfig';
+import schemas from 'redux/nodes/entities/base/schemas';
+import { parseEntityFunc } from 'redux/nodes/entities/hosts/helpers';
 
 const { HOSTS: schema } = schemas;
 
@@ -10,25 +9,6 @@ export default reduxConfig({
   destroyFunc: Kolide.hosts.destroy,
   entityName: 'hosts',
   loadAllFunc: Kolide.hosts.loadAll,
-  parseEntityFunc: (host) => {
-    const { network_interfaces: networkInterfaces } = host;
-    const networkInterface = networkInterfaces && find(networkInterfaces, { id: host.primary_ip_id });
-    const clockSpeed = host.cpu_brand.split('@ ')[1] || host.cpu_brand.split('@')[1];
-
-    const additionalAttrs = {
-      host_cpu: `${host.cpu_physical_cores} x ${clockSpeed}`,
-      target_type: 'hosts',
-    };
-
-    if (networkInterface) {
-      additionalAttrs.host_ip_address = networkInterface.address;
-      additionalAttrs.host_mac = networkInterface.mac;
-    }
-
-    return {
-      ...host,
-      ...additionalAttrs,
-    };
-  },
+  parseEntityFunc,
   schema,
 });
