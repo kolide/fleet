a157bf619be7512a9b00b61990cad1dac07b74bc
Mike Stone <mike@gnar.dog>
2017-02-06 11:41:47 -0500

    Allow updating a scheduled query

--- frontend/components/side_panels/ScheduleQuerySidePanel/ScheduleQuerySidePanel.jsx
+++ frontend/components/side_panels/ScheduleQuerySidePanel/ScheduleQuerySidePanel.jsx
@@ -2,23 +2,35 @@ import React, { PropTypes } from 'react';
 
 import ConfigurePackQueryForm from 'components/forms/ConfigurePackQueryForm';
 import queryInterface from 'interfaces/query';
+import scheduledQueryInterface from 'interfaces/scheduled_query';
 import SearchPackQuery from './SearchPackQuery';
 import SecondarySidePanelContainer from '../SecondarySidePanelContainer';
 
 const baseClass = 'schedule-query-side-panel';
 
-const ScheduleQuerySidePanel = ({ allQueries, onConfigurePackQuerySubmit, onSelectQuery, selectedQuery }) => {
+const ScheduleQuerySidePanel = ({
+  allQueries,
+  onConfigurePackQuerySubmit,
+  onUpdateScheduledQuery,
+  onSelectQuery,
+  selectedQuery,
+  selectedScheduledQuery,
+}) => {
   const renderForm = () => {
     if (!selectedQuery) {
       return false;
     }
 
-    const formData = { query_id: selectedQuery.id };
+    const formData = selectedScheduledQuery || {};
+
+    formData.query_id = selectedQuery.id;
+
+    const handleSubmit = selectedScheduledQuery ? onUpdateScheduledQuery : onConfigurePackQuerySubmit;
 
     return (
       <ConfigurePackQueryForm
         formData={formData}
-        handleSubmit={onConfigurePackQuerySubmit}
+        handleSubmit={handleSubmit}
       />
     );
   };
@@ -39,7 +51,9 @@ ScheduleQuerySidePanel.propTypes = {
   allQueries: PropTypes.arrayOf(queryInterface),
   onConfigurePackQuerySubmit: PropTypes.func,
   onSelectQuery: PropTypes.func,
+  onUpdateScheduledQuery: PropTypes.func,
   selectedQuery: queryInterface,
+  selectedScheduledQuery: scheduledQueryInterface,
 };
 
 export default ScheduleQuerySidePanel;
