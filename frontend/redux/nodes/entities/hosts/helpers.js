9e84ffcbfc0425a0d21867f48767bb7a68b84df9
Kyle Knight <kyle@kolide.co>
2017-03-03 09:16:25 -0600

    Updating lint and test

--- frontend/redux/nodes/entities/hosts/helpers.js
+++ frontend/redux/nodes/entities/hosts/helpers.js
@@ -4,7 +4,7 @@ export const parseEntityFunc = (host) => {
   const { network_interfaces: networkInterfaces } = host;
   const networkInterface = networkInterfaces && find(networkInterfaces, { id: host.primary_ip_id });
   const clockSpeed = host.cpu_brand.split('@ ')[1] || host.cpu_brand.split('@')[1];
-  const clockSpeedFlt = parseFloat(clockSpeed.split('GHz')[0].trim())
+  const clockSpeedFlt = parseFloat(clockSpeed.split('GHz')[0].trim());
 
   const additionalAttrs = {
     host_cpu: `${host.cpu_physical_cores} x ${Math.floor(clockSpeedFlt * 10) / 10} GHz`,
