80e7799a1167a86ba02397e7addadeda7b76e288
John Murphy <john@kolide.co>
2017-04-12 10:14:58 -0500

    Datastore support for SSO

--- server/datastore/mysql/migrations/tables/20170411155225_CreateTableIdentityProviders.go
+++ server/datastore/mysql/migrations/tables/20170411155225_CreateTableIdentityProviders.go
@@ -20,6 +20,7 @@ func Up_20170411155225(tx *sql.Tx) error {
 			"`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
 			"`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
 			"`deleted_at` timestamp NULL DEFAULT NULL, " +
+			"`deleted` tinyint(1) NOT NULL DEFAULT FALSE, " +
 			"PRIMARY KEY (`id`), " +
 			"UNIQUE KEY `idx_unique_identity_providers_name` (`name`) USING BTREE " +
 			") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
