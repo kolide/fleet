416932eab18bf63791219cf588aa24805152333e
Mike Arpaia <mike@arpaia.co>
2017-10-17 19:55:40 -0600

    updates

--- tools/ci/docker/aptly/update.sh
+++ tools/ci/docker/aptly/update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 snapshot="$(date +%s)"
+aptly repo create fleet
 aptly repo add fleet /deb
 aptly snapshot create "${snapshot}" from repo fleet
 aptly publish drop jessie
-aptly publish -gpg-key="000CF27C" --distribution="jessie" snapshot "${snapshot}"
+aptly publish -gpg-key="10E85AFB" --distribution="jessie" snapshot "${snapshot}"
