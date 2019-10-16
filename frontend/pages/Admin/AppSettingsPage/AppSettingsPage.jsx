e6e5551577fdbec49d345e09027f196db3cf736b
Austin Burnett <austburn@gmail.com>
2019-10-16 13:48:10 -0500

    drop file from changes - just whitespace

--- frontend/pages/Admin/AppSettingsPage/AppSettingsPage.jsx
+++ frontend/pages/Admin/AppSettingsPage/AppSettingsPage.jsx
@@ -34,6 +34,7 @@ class AppSettingsPage extends Component {
   onFormSubmit = (formData) => {
     const { appConfig, dispatch } = this.props;
     const diff = deepDifference(formData, appConfig);
+
     dispatch(updateConfig(diff))
       .then(() => {
         dispatch(renderFlash('success', 'Settings updated!'));
